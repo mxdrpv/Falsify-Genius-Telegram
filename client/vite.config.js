675554df18354d08d116159f9ea5ff05e34332e4
mxdrpv <mxdrpv2@gmail.com>
2025-04-16 03:28:01 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',  // указываем, что index.html лежит в public
   build: {
     outDir: 'dist',  // куда будут собраны файлы
+	rollupOptions: {
+		input: path.resolve(__dirname, 'public/index.html'),  // Указываем точку входа
+    },
   },
 })
