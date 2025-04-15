c46ef1806b64ca88a486a71547465f33e1ab2d45
mxdrpv <mxdrpv2@gmail.com>
2025-04-16 02:38:31 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'public/index.html')
-      }
-    }
+      input: path.resolve(__dirname, 'public/index.html'),
+    },
   },
-  publicDir: 'public',
-  root: './'
 })
