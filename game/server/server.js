f889fbcce89118fcd3a0fbe67e759958d78fa9dd
mxdrpv <mxdrpv2@gmail.com>
2025-04-16 06:26:42 +0700

    Update server.js

--- game/server/server.js
+++ game/server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import startBot from '../bot/bot.js';
 
+const app = express();
 const port = process.env.PORT || 10000;
+const MINIAPP_URL = process.env.MINIAPP_URL || `${process.env.RENDER_EXTERNAL_URL}/game`;
 
-app.use(express.static(path.join(__dirname, '../miniapp')));
+dotenv.config();
+
+app.use('/game', express.static(path.join(__dirname, 'miniapp')));
+
+app.get('/api/game', (req, res) => {
+  res.json({ status: 'game server running', miniappUrl: process.env.MINIAPP_URL });
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../miniapp/views/index.html'));
@@ -13,6 +25,8 @@ app.get('/', (req, res) => {
 app.get('/start_round', (req, res) => {
   res.json({ question: 'Какой ваш любимый цвет?' });  // Просто пример вопроса
 });
+
+startBot();
 
 app.listen(port, () => {
   console.log(`Сервер работает на порту ${port}`);
